8e2bf3faac6ec52e222945566788385b0c29d614
Nemanja <nemadjo94@gmail.com>
2019-07-09 10:38:57 +0200

    Add files via upload

---
+++ AspTutorialMVC/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspTutorialMVC.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public List<Product> Products { get; set; }
+        public decimal Total { get; set; }
+        public decimal Discount => Total > 1000M ? Total * 0.1M : Total;
+    }
+}
